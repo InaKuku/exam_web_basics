39ca6a850337ef0ba82ae730cb0b778274cb6503
InaKuku <82227246+InaKuku@users.noreply.github.com>
2022-04-22 16:07:43 +0300

    Delete Python Web Basics directory

--- Python Web Basics/Exam_Prep_I/Exam_Prep_I/job/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from Exam_Prep_I.job.models import Profile
-
-
-@admin.register(Profile)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display=('id', 'first_name', 'last_name', 'budget')
